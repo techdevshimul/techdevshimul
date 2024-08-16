556accbfec50e87e9808c380b8a09b9f9f77cdc0
Shimul Hossain <shimultechtips@gmail.com>
2024-08-16 15:09:03 +0600

    2024-08-16 15:08

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="p-2 mx-auto max-w-screen-xl ">
-      <div className="flex mx-3 py-7 flex-col justify-center gap-24 sm:flex-row">
-        <div className="flex flex-col justify-center gap-3">
+      <div className="flex gap-9 justify-between p-6">
+        <div className="flex flex-col justify-center gap-3 max-w-screen-sm">
           <p className="text-xl">Hi, my name is</p>
-          <h2 className="text-5xl">Shimul Hossain</h2>
+          <h2 className="text-5xl font-bold">Shimul Hossain</h2>
           <p className="text-3xl">
             Welcome to techdevshimul portfolio website.
           </p>
@@ -25,15 +25,8 @@ export default function Home() {
             Download my resume
           </Link>
         </div>
-        <div className="flex justify-center min-h-80 min-w-80">
-          <Image
-            className="rounded-full border-white border-8"
-            src={photo}
-            alt="Shimul Hossain Photo"
-            priority
-            width={300}
-            height={300}
-          />
+        <div className="image-profile">
+          <img src={photo.src} alt="Shimul Hossain Photo" />
         </div>
       </div>
     </main>
--- components/main-header.tsx
+++ components/main-header.tsx
@@ -4,15 +4,17 @@ export default function MainHeader() {
   return (
     <>
       <nav className="max-w-screen-xl mx-auto">
-        <div className="flex justify-between mx-10 items-center">
+        <div className="flex justify-between mx-10 items-center flex-wrap">
           <Link href="/">
             <div className="flex items-center gap-2">
               <h1 className="text-xl font-bold">TechDevShimul</h1>
             </div>
           </Link>
-          <ul className="flex flex-row justify-center gap-5 font-bold my-5">
+          <ul className="flex flex-wrap flex-row justify-center gap-5 my-5">
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/" className="font-bold">
+                Home
+              </Link>
             </li>
             <li>
               <Link href="/projects">Projects</Link>
